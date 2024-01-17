03625807bb06126ff64c8b797ee435c1db4169cc
Vincent Palomo <vincentpalomo@yahoo.com>
2024-01-16 20:10:51 -0600

    updated message to who gained the critical damage

--- 0.0.2/character.py
+++ 0.0.2/character.py
@@ -1,48 +1,54 @@
 import random
 
+
 class Character:
-  def __init__(self, name, health, damage, weapon=None) -> None:
-    self.name = name
-    self.health = health
-    self.max_health = health
-    self.damage = damage
-    self.weapon = weapon
+    def __init__(self, name, health, damage, weapon=None) -> None:
+        self.name = name
+        self.health = health
+        self.max_health = health
+        self.damage = damage
+        self.weapon = weapon
 
-  def attack(self) -> None:
-    is_critical = random.random() < 0.1
+    def attack(self) -> None:
+        is_critical = random.random() < 0.1
 
-    if is_critical:
-      print(f'!CRITICAL HIT!')
-      print()
-      return int(self.damage * 1.5)
-    else:
-      return random.randint(1, self.damage)
-    
-  def take_damage(self, damage) -> None:
-    self.health -= damage
+        if is_critical:
+            print(f'!CRITICAL HIT!')
+            print(f'{self.name} gained extra damage!')
+            print()
 
-    if self.health < 0:
-      self.health = 0
+            return int(self.damage * 1.5)
+        else:
+            return random.randint(1, self.damage)
 
-  def is_alive(self) -> None:
-    return self.health > 0
-  
-  def display_health(self):
-    bar_length = 20
-    remaining_health = int((self.health / 100) * bar_length)
-    health_bar = '[' + '#' * remaining_health + '-' * (bar_length - remaining_health) + ']'
-    print(f'{self.name}`s HP: {health_bar} {self.health}')
+    def take_damage(self, damage) -> None:
+        self.health -= damage
 
-  def reset(self) -> None:
-    self.health = self.max_health
+        if self.health < 0:
+            self.health = 0
+
+    def is_alive(self) -> None:
+        return self.health > 0
+
+    def display_health(self):
+        bar_length = 20
+        remaining_health = int((self.health / 100) * bar_length)
+        health_bar = '|' + '🀫' * remaining_health + \
+            '-' * (bar_length - remaining_health) + '|'
+        print(f'{self.name}`s HP: {health_bar} {self.health}')
+
+    def reset(self) -> None:
+        self.health = self.max_health
+
 
 class Hero(Character):
-  def __init__(self, name, health, damage, weapon=None) -> None:
-    super().__init__(name, health, damage, weapon)
+    def __init__(self, name, health, damage, weapon=None) -> None:
+        super().__init__(name, health, damage, weapon)
 
-  def reset(self):
-    super().reset()
+    def reset(self):
+        super().reset()
+
 
 class Enemy(Character):
-  def __init__(self, name, health, damage, weapon=None) -> None:
-    super().__init__(name, health, damage, weapon)
+    def __init__(self, name, health, damage, weapon=None) -> None:
+        super().__init__(name, health, damage, weapon)
--- 0.0.2/main.py
+++ 0.0.2/main.py
@@ -2,78 +2,83 @@ import time
 import os
 from character import Hero, Enemy
 
+
 class GameLoop:
-  def __init__(self, hero, enemy) -> None:
-    self.hero = hero
-    self.enemy = enemy
+    def __init__(self, hero, enemy) -> None:
+        self.hero = hero
+        self.enemy = enemy
 
-  def battle(self):
-    os.system('clear')
-    print()
-    print(f'{self.hero.name} vs {self.enemy.name}')
-    print()
-    print(f'Prepare for battle!')
-    time.sleep(2)
-
-    while self.hero.is_alive() and self.enemy.is_alive():
-      os.system('clear')
-      print()
-      self.hero.display_health()
-      self.enemy.display_health()
-
-      print()
-      hero_damage = self.hero.attack()
-      enemy_damage = self.enemy.attack()
-
-      self.enemy.take_damage(hero_damage)
-      print(f'{self.hero.name} deals {hero_damage} to {self.enemy.name}!')
-      time.sleep(1)
-
-      if not self.enemy.is_alive():
+    def battle(self):
         os.system('clear')
         print()
-        print(f'{self.enemy.name} has been defeated! 💀')
+        print(f'{self.hero.name} vs {self.enemy.name}')
+        print()
+        print(f'Prepare for battle!')
         time.sleep(2)
-        break
 
-      self.hero.take_damage(enemy_damage)
-      print(f'{self.enemy.name} deals {enemy_damage} to {self.hero.name}!')
-      time.sleep(1)
+        while self.hero.is_alive() and self.enemy.is_alive():
+            os.system('clear')
+            print()
+            self.hero.display_health()
+            self.enemy.display_health()
 
-      if not self.hero.is_alive():
+            print()
+            hero_damage = self.hero.attack()
+            enemy_damage = self.enemy.attack()
+            time.sleep(1)
+
+            self.enemy.take_damage(hero_damage)
+            print(f'{self.hero.name} deals {
+                  hero_damage} to {self.enemy.name}!')
+            time.sleep(1)
+
+            if not self.enemy.is_alive():
+                os.system('clear')
+                print()
+                print(f'{self.enemy.name} has been defeated! 💀')
+                time.sleep(2)
+                break
+
+            self.hero.take_damage(enemy_damage)
+            print(f'{self.enemy.name} deals {
+                  enemy_damage} to {self.hero.name}!')
+            time.sleep(1)
+
+            if not self.hero.is_alive():
+                os.system('clear')
+                print()
+                print(f'{self.hero.name} has been defeated! 💀')
+                time.sleep(2)
+                break
+
+            print()
+            input('Press any button to continue...')
+
         os.system('clear')
         print()
-        print(f'{self.hero.name} has been defeated! 💀')
-        time.sleep(2)
-        break
-      
-      print()
-      input('Press any button to continue...')
+        print(f'Stage Complete!')
+        print()
+        self.hero.display_health()
+        self.enemy.display_health()
 
-    os.system('clear')
-    print()
-    print(f'Stage Complete!')
-    print()
-    self.hero.display_health()
-    self.enemy.display_health()
+    def start_game(self):
+        while True:
+            os.system('clear')
+            print()
+            print('Welcome to Heroes of Jinxton ⚔')
+            print()
+            input('Press Enter to Start!')
+            self.battle()
 
-  def start_game(self):
-    while True:
-      os.system('clear')
-      print()
-      print('Welcome to Heroes of Jinxton ⚔')
-      print()
-      input('Press Enter to Start!')
-      self.battle()
+            print()
+            play_again = input('Do you want to play again? (yes/no): ')
+            if play_again.lower() != 'yes':
+                break
+            else:
+                os.system('clear')
+                self.hero.reset()
+                self.enemy.reset()
 
-      print()
-      play_again = input('Do you want to play again? (yes/no): ')
-      if play_again.lower() != 'yes':
-        break
-      else:
-        os.system('clear')
-        self.hero.reset()
-        self.enemy.reset()
 
 hero = Hero(name='Doo', health=100, damage=25)
 enemy = Enemy(name='Wormy', health=100, damage=25)
