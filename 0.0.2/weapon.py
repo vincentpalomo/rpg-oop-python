b8ae053ead08203841c70cc9950006bb7b98a934
Vincent Palomo <vincentpalomo@yahoo.com>
2024-01-17 18:50:48 -0600

    created weapon class

---
+++ 0.0.2/weapon.py
@@ -0,0 +1,20 @@
+class Weapon:
+  def __init__(self, name: str, weapon_type: str, damage: int) -> None:
+    self.name = name
+    self.weapon_type = weapon_type
+    self.damage = damage
+
+# swords
+iron_sword = Weapon(name='iron sword', weapon_type='sword', damage=15)
+bronze_sword = Weapon(name='bronze sword', weapon_type='sword', damage=25)
+silver_sword = Weapon(name='silver sword', weapon_type='sword', damage=45)
+
+# bows
+short_bow = Weapon(name='short bow', weapon_type='ranged', damage=10)
+long_bow = Weapon(name='long bow', weapon_type='ranged', damage=20)
+recurve_bow = Weapon(name='recurve bow', weapon_type='ranged', damage=35)
+
+# blunt
+fists = Weapon(name='fists', weapon_type='blunt', damage=5)
+bronze_hammer = Weapon(name='bronze hammer', weapon_type='blunt', damage=15)
+morning_star = Weapon(name='morning star', weapon_type='blunt', damage=40)
