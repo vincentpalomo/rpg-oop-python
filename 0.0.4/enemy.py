c66d1069fa52ea4f2d629be108b9555f6987c0af
Vincent Palomo <vincentpalomo@yahoo.com>
2024-10-05 20:53:45 -0500

    updated else if statement for enemies and bosses. enemies pass level 60 will be random from easy to spectral

--- 0.0.4/enemy.py
+++ 0.0.4/enemy.py
@@ -1,5 +1,5 @@
 import random
-from character import Enemy, enemies, easy_enemies, medium_enemies, hard_enemies, elite_enemies, bosses
+from character import Enemy, enemies, easy_enemies, medium_enemies, hard_enemies, elite_enemies, mystic_enemies, spectral_enemies, bosses
 
 class Random_Enemy:
     def __init__(self) -> None:
@@ -10,29 +10,40 @@ class Random_Enemy:
         if not self.enemies:
             return None
         
-        if stage < 10:
-            self.enemies = easy_enemies
-        elif stage == 10:
-            self.enemies = bosses
-        elif stage > 10 and stage < 20:
-            self.enemies = medium_enemies
-        elif stage == 20:
-            self.enemies = bosses
-        elif stage > 20 and stage < 30:
-            self.enemies = hard_enemies
-        elif stage == 30:
-            self.enemies = bosses
-        elif stage > 30 and stage < 40:
-            self.enemies = elite_enemies
-        elif stage == 40:
-            self. enemies = bosses
-
-        # if stage % 10 == 0:
-        #     self.enemies = bosses
-        # elif stage < 10:
+        # if stage < 10:
         #     self.enemies = easy_enemies
-        # elif stage < 20:
+        # elif stage == 10:
+        #     self.enemies = bosses
+        # elif stage > 10 and stage < 20:
         #     self.enemies = medium_enemies
+        # elif stage == 20:
+        #     self.enemies = bosses
+        # elif stage > 20 and stage < 30:
+        #     self.enemies = hard_enemies
+        # elif stage == 30:
+        #     self.enemies = bosses
+        # elif stage > 30 and stage < 40:
+        #     self.enemies = elite_enemies
+        # elif stage == 40:
+        #     self. enemies = bosses
+
+        if stage % 10 == 0:
+            self.enemies = bosses
+        elif stage < 10:
+            self.enemies = easy_enemies
+        elif 10 < stage < 20:
+            self.enemies = medium_enemies
+        elif 20 < stage < 30:
+            self.enemies = hard_enemies
+        elif 30 < stage < 40:
+            self.enemies = elite_enemies
+        elif 40 < stage < 50:
+            self.enemies = mystic_enemies
+        elif 50 < stage < 60:
+            self.enemies = spectral_enemies
+        elif stage >= 60:
+            enemy_tiers = [easy_enemies, medium_enemies, hard_enemies, elite_enemies, mystic_enemies, spectral_enemies]
+            self.enemies = enemy_tiers[(stage // 10) % len(enemy_tiers)]
 
         random_enemy = random.choice(self.enemies)
         return Enemy(
